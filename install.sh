99ad31c52dcb712c4e70b829ed2523f3e384bfe5
Jac <jac@jacbeekers.nl>
2019-11-02 19:40:58 +0100

    __version__ = "0.0.9" now on pypi.

---
+++ install.sh
@@ -0,0 +1,5 @@
+pip uninstall -y informatica-airflow-plugin
+pip uninstall -y devops-informatica-tools
+pip install informatica-airflow-plugin --extra-index-url https://testpypi.python.org/pypi
+pip install devops-informatica-tools --extra-index-url https://testpypi.python.org/pypi
+
