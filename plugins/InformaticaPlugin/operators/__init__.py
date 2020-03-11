b2da1f434bb34e3bdc4d918f6483afcb36a181e2
Jac <jac.beekers@nl.abnamro.com>
2020-03-11 18:30:14 +0100

    improve flexibility

---
+++ plugins/InformaticaPlugin/operators/__init__.py
@@ -0,0 +1,6 @@
+available_arguments = {
+    "application_name": "-a"
+    ,"mapping_name": "-m"
+    ,"os_profile": "--osprofile"
+    ,"as_is_arguments": "--extra"
+}
--- plugins/InformaticaPlugin/operators/execute_mapping.py
+++ plugins/InformaticaPlugin/operators/execute_mapping.py
@@ -2,14 +2,21 @@ from airflow.models import BaseOperator
 from airflow import utils as airflow_utils, AirflowException
 
 from execution import runMapping
+from InformaticaPlugin.operators import available_arguments
 
 
 class ExecuteMapping(BaseOperator):
 
     @airflow_utils.apply_defaults
-    def __init__(self, application_name, mapping_name, **kwargs):
-        self.application_name = application_name
-        self.mapping_name = mapping_name
+    def __init__(self, **kwargs):
+        self.infa_arguments = []
+        for key, value in kwargs.items():
+            if key in available_arguments:
+                self.infa_arguments.append(available_arguments[key] + " " + '"' + value + '"')
+            else:
+                print(
+                    "unrecognized key >" + key + "< provided. Its provided value is >" + value + "<. Argument IGNORED.")
+
         super(ExecuteMapping, self).__init__(
             **kwargs)
 
@@ -18,16 +25,10 @@ class ExecuteMapping(BaseOperator):
         print("dag_id: " + self.dag_id)
         print("task_type: " + self.task_type)
         print("task id: " + self.task_id)
-        print("application_name: " + self.application_name)
-        print("mapping_name: " + self.mapping_name)
-        arguments = [
-            "-a",
-            self.application_name,
-            "-m",
-            self.mapping_name,
-            # TODO: the others to be added
-        ]
-        infa = runMapping.ExecuteInformaticaMapping(arguments, False)
+        print("infa_arguments count: " + str(self.infa_arguments.count()))
+        print("infa_arguments: " + self.infa_arguments)
+
+        infa = runMapping.ExecuteInformaticaMapping(self.infa_arguments, False)
         result = infa.runit(infa.arguments)
         if result.rc != 0:
-            raise AirflowException("RunMapping failed.")
+            raise AirflowException("RunMapping failed: " + result.message)
