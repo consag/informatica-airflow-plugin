71fea57a7c31a0c96be481be2450e6f8b87d5a8b
Jac <jac@jacbeekers.nl>
2020-03-20 23:17:32 +0100

    scorecard supports dynamic environment

--- plugins/InformaticaPlugin/operators/execute_scorecard.py
+++ plugins/InformaticaPlugin/operators/execute_scorecard.py
@@ -2,12 +2,22 @@ from airflow.models import BaseOperator
 from airflow import utils as airflow_utils, AirflowException
 
 from execution import runScorecard
+from InformaticaPlugin.operators import available_arguments
+import os
 
 class ExecuteScorecard(BaseOperator):
 
     @airflow_utils.apply_defaults
     def __init__(self, scorecard_path, **kwargs):
-        self.scorecard_path = scorecard_path
+        self.infa_arguments = []
+        self.pre_command = None
+        for key, value in kwargs.items():
+            if key == 'target':
+                self.pre_command = '. ' + os.environ.get('configDir', '.') + '/scheduler_env.' + value + '.sh'
+            else:
+                if key in available_arguments:
+                    self.infa_arguments.append(available_arguments[key] + " " + value)
+
         super(ExecuteScorecard, self).__init__(
             **kwargs)
 
@@ -16,13 +26,15 @@ class ExecuteScorecard(BaseOperator):
         print("dag_id: " + self.dag_id)
         print("task_type: " + self.task_type)
         print("task id: " + self.task_id)
-        print("scorecard_path: " + self.scorecard_path)
-        arguments = [
-            "-s",
-            self.scorecard_path,
-            # TODO: the others to be added
-        ]
-        infa = runScorecard.ExecuteInformaticaScorecard(arguments, False)
+        print("infa_arguments: " + ' '.join(self.infa_arguments))
+        if self.pre_command is None:
+            print("no pre_command provided.")
+        else:
+            print("pre_command: " + self.pre_command)
+
+        infa = runScorecard.ExecuteInformaticaScorecard(self.infa_arguments, log_on_console=False,
+                                                        pre_command=self.pre_command)
+
         result = infa.runit(infa.arguments)
         if result.rc != 0:
-            raise AirflowException("RunScorecard failed.")
+            raise AirflowException("RunScorecard failed: " + result.message)
